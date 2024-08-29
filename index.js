f94a8e9e10b1fac93d96e42252ddf74bdd6f600d
SaraFruergaard <145442999+SaraFruergaard@users.noreply.github.com>
2024-08-29 21:22:47 +0200

    init

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
+const serverless = require("serverless-http");
 const app = express();
-const port = 3000;
 
 app.use("/", express.static("public"));
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+export const handler = serverless(app);
